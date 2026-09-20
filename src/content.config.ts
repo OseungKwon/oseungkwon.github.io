95d25f303b1e66a65f218183f3fabb4a8276add2
OseungKwon <00osos@naver.com>
2026-09-20 23:25:02 +0900

    feat: 블로그 글에 subtitle 구조 추가

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,6 +6,7 @@ const blog = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
+      subtitle: z.string().optional(),
       description: z.string(),
       pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
